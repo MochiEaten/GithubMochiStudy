f17f9a256662f74b3ece3e69102f7d7b2a3e7f5d
MochiEaten <x13515523060@163.com>
2024-07-31 21:52:44 +0800

    第二次提交（测试一下）

--- src/main/java/com/mochi/demos/web/UserController.java
+++ src/main/java/com/mochi/demos/web/UserController.java
@@ -9,6 +9,6 @@ package com.mochi.demos.web;
  **/
 public class UserController {
     public static void main(String[] args) {
-        System.out.println("测试一下");
+        System.out.println("测试第二下");
     }
 }
